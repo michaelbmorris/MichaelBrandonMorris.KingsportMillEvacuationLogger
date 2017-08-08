05355eb49e36f05f7b74dc144931686c3767726b
Michael Brandon Morris <brandon@michaelbmorris.com>
2017-08-08 06:16:04 -0400

    Changed order of evacuation statuses.

--- MichaelBrandonMorris.KingsportMillEvacuationLogger/Models/UserEvacuationStatus.cs
+++ MichaelBrandonMorris.KingsportMillEvacuationLogger/Models/UserEvacuationStatus.cs
@@ -1,10 +1,33 @@
 ﻿namespace MichaelBrandonMorris.KingsportMillEvacuationLogger.Models
 {
+    /// <summary>
+    ///     Enum UserEvacuationStatus
+    /// </summary>
+    /// TODO Edit XML Comment Template for UserEvacuationStatus
     public enum UserEvacuationStatus
     {
+        /// <summary>
+        ///     The missing
+        /// </summary>
+        /// TODO Edit XML Comment Template for Missing
+        Missing,
+
+        /// <summary>
+        ///     The unknown
+        /// </summary>
+        /// TODO Edit XML Comment Template for Unknown
         Unknown,
+
+        /// <summary>
+        ///     The here
+        /// </summary>
+        /// TODO Edit XML Comment Template for Here
         Here,
-        Away,
-        Missing
+
+        /// <summary>
+        ///     The away
+        /// </summary>
+        /// TODO Edit XML Comment Template for Away
+        Away
     }
 }
